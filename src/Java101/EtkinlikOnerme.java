0b3eeecd354ddba7ada9e1b244cde865b64714fb
Berkanturkoglu48 <berkanturkoglubilgisayar@gmail.com>
2023-11-14 20:36:24 +0300

    Mini ödev eklendi . (Etkinlik Önerme)

---
+++ src/Java101/EtkinlikOnerme.java
@@ -0,0 +1,25 @@
+package Java101;
+import java.util.Scanner;
+
+public class EtkinlikOnerme {
+    public static void main(String[] args) {
+        int derece;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Hava sıcaklığını giriniz: ");
+        derece = input.nextInt();
+
+        if (derece < 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        }
+        else if (derece <= 15) {
+            System.out.println("Sinemaya gidebilirsiniz.");
+        }
+        else if (derece <= 25) {
+            System.out.println("Pikniğe gidebilirsiniz.");
+        }
+        else {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
