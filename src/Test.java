038579b7916c0aeac0162d27131e337e3a96823e
Berkanturkoglu48 <berkanturkoglubilgisayar@gmail.com>
2023-11-15 10:36:24 +0300

    Mini ödev eklendi . (3 ve 4 e Bölünenlerin Hesaplanması)

--- src/Test.java
+++ src/Test.java
@@ -3,21 +3,35 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int secim;
+        String kAdi, kSifre;
 
+        System.out.print("Kullanıcı adınızı giriniz: ");
+        kAdi = input.nextLine();
 
-        System.out.println("Sayı Giriniz");
-        String adSoyad = input.nextLine();
-        System.out.println(adSoyad);
+        System.out.print("Şifrenizi giriniz: ");
+        kSifre = input.nextLine();
 
+        if (kAdi.equals("patika") && kSifre.equals("patika")) {
+            System.out.println("Giriş Başarılı.");
+        } else if (kAdi.equals("patika")) {
+            System.out.println("Şifrenizi hatalı girdiniz.\nSıfırlamak istiyorsanız 1 çıkış yapmak istiyorsanız 2'yi tuşlayın.");
+            secim = input.nextInt();
 
+            if (secim == 1) {
+                System.out.println("Yeni şifrenizi giriniz");
+                kSifre = input.next();  // nextLine yerine next kullanabilirsiniz
 
-        // sa
-        /* saasdasdads
-        asdasdasdasd
-        asdasdasdasd
-         */
+                if (kSifre.equals("patika")) {
+                    System.out.println("Eskiden kullandığınız bir şifreyi tekrar kullanamazsınız!");
+                } else {
+                    System.out.println("Şifre değiştirme işlemi başarılı");
+                }
+            } else {
+                System.out.println("Çıkış yapılıyor...");
+            }
+        } else {
+            System.out.println("Hatalı giriş yaptınız");
         }
-
-
-
+    }
 }
